d690ac2d8f006b5e1d69b3da057df8c21ba0d332
Willy Wilsen <13520160@std.stei.itb.ac.id>
2024-03-10 23:40:48 +0700

    ECB Cipher

--- src/api/app.py
+++ src/api/app.py
@@ -2,61 +2,81 @@ import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils.constant import METHOD
+from utils.ECB.service import extend_bit_ecb, encrypt_ecb, decrypt_ecb
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/encrypt', methods=['POST'])
 def encrypt():
-  try:
-    # Input
-    if request.form:
-      data = request.form.to_dict()
-      file = request.files['inputFile']
-    else:
-      data = request.get_json()
+  # Input
+  if request.form:
+    data = request.form.to_dict()
+    file = request.files['inputFile']
+  else:
+    data = request.get_json()
+    # Convert text to bit
+    data['inputBit'] = ''.join(format(ord(char), '08b') for char in data['inputText'])
+    print(data['inputBit'])
 
-    # Output
-    if data['method'] == METHOD['ECB']:
-      data['result'] = 'ECB'
-    elif data['method'] == METHOD['CBC']:
-      data['result'] = 'CBC'
-    elif data['method'] == METHOD['OFB']:
-      data['result'] = 'OFB'
-    elif data['method'] == METHOD['CFB']:
-      data['result'] = 'CFB'
-    elif data['method'] == METHOD['COUNTER']:
-      data['result'] = 'Counter'
+  # Output
+  # Convert key to bit
+  data['keyBit'] = ''.join(format(ord(char), '08b') for char in data['key'])
+  # ECB
+  if data['method'] == METHOD['ECB']:
+    # Extend plainteks bit
+    data['inputBit'] = extend_bit_ecb(data['inputBit'], data['keyBit'])
+    # Encrypt plainteks bit
+    data['resultBit'] = encrypt_ecb(data['inputBit'], data['keyBit'])
+    # Convert cipher bit to text
+    data['result'] = ''
+    for i in range(0, len(data['resultBit']), 8):
+      data['result'] += chr(int(data['resultBit'][i:i+8], 2))
+  elif data['method'] == METHOD['CBC']:
+    data['result'] = 'CBC'
+  elif data['method'] == METHOD['OFB']:
+    data['result'] = 'OFB'
+  elif data['method'] == METHOD['CFB']:
+    data['result'] = 'CFB'
+  elif data['method'] == METHOD['COUNTER']:
+    data['result'] = 'Counter'
 
-    return jsonify(data)
-  except Exception as e:
-    return jsonify({'error': str(e)}), 500
+  return jsonify(data)
 
 @app.route('/api/decrypt', methods=['POST'])
 def decrypt():
-  try:
-    # Input
-    if request.form:
-      data = request.form.to_dict()
-      file = request.files['inputFile']
-    else:
-      data = request.get_json()
+  # Input
+  if request.form:
+    data = request.form.to_dict()
+    file = request.files['inputFile']
+  else:
+    data = request.get_json()
+    # Convert text to bit
+    data['inputBit'] = ''.join(format(ord(char), '08b') for char in data['inputText'])
 
-    # Output
-    if data['method'] == METHOD['ECB']:
-      data['result'] = 'ECB'
-    elif data['method'] == METHOD['CBC']:
-      data['result'] = 'CBC'
-    elif data['method'] == METHOD['OFB']:
-      data['result'] = 'OFB'
-    elif data['method'] == METHOD['CFB']:
-      data['result'] = 'CFB'
-    elif data['method'] == METHOD['COUNTER']:
-      data['result'] = 'Counter'
-    
-    return jsonify(data)
-  except Exception as e:
-    return jsonify({'error': str(e)}), 500
+  # Output
+  # Convert key to bit
+  data['keyBit'] = ''.join(format(ord(char), '08b') for char in data['key'])
+  # ECB
+  if data['method'] == METHOD['ECB']:
+    # Extend plainteks bit
+    data['inputBit'] = extend_bit_ecb(data['inputBit'], data['keyBit'])
+    # Decrypt plainteks bit
+    data['resultBit'] = decrypt_ecb(data['inputBit'], data['keyBit'])
+    # Convert cipher bit to text
+    data['result'] = ''
+    for i in range(0, len(data['resultBit']), 8):
+      data['result'] += chr(int(data['resultBit'][i:i+8], 2))
+  elif data['method'] == METHOD['CBC']:
+    data['result'] = 'CBC'
+  elif data['method'] == METHOD['OFB']:
+    data['result'] = 'OFB'
+  elif data['method'] == METHOD['CFB']:
+    data['result'] = 'CFB'
+  elif data['method'] == METHOD['COUNTER']:
+    data['result'] = 'Counter'
+
+  return jsonify(data)
 
 if __name__ == '__main__':
   app.run()
