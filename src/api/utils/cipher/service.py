15c7be84ad90ca530f9d460b37c85f158e2d3342
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-03-22 15:23:37 +0700

    feat: Encryption and Decryption

---
+++ src/api/utils/cipher/service.py
@@ -0,0 +1,33 @@
+from subkey import generate_sub_keys
+from feistel import feistel_network
+
+def encrypt(block_plaintext: int, key: int) -> int:
+    data_len = len(f"{block_plaintext:08b}")
+    assert data_len <= 128, f"Input size {data_len}, expected less than 128"
+    sub_keys = generate_sub_keys(key, 16)
+    ciphertext = feistel_network(block_plaintext, sub_keys)
+    return ciphertext
+
+def decrypt(block_ciphertext:int, key: int) -> int:
+    data_len = len(f"{block_ciphertext:08b}")
+    assert data_len <= 128, f"Input size {data_len}, expected less than 128"
+    sub_keys = generate_sub_keys(key, 16)
+    plaintext = feistel_network(block_ciphertext, sub_keys, decrypt=True)
+    return plaintext
+
+def test():
+    plaintext = 20030626
+    key = "alifyasa"
+
+    formatted_key = int(''.join(format(ord(char), '08b') for char in key), 2)
+    print(f"Plaintext : {plaintext:0128b}")
+    print(f"Key       : {formatted_key:0128b}")
+
+    ciphertext = encrypt(plaintext, formatted_key)
+    print(f"Ciphertext: {ciphertext:0128b}")
+
+    decrypted_ciphertext = decrypt(ciphertext, formatted_key)
+    print(f"Decrypted : {decrypted_ciphertext:0128b}")
+
+if __name__ == "__main__":
+    test()
